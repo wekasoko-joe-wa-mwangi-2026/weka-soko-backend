6b0670f34dd15371355334ae2ec06088a08f297e
joewamwangi <105190959+joewamwangi@users.noreply.github.com>
2026-02-26 17:26:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -160,6 +160,7 @@ io.on("connection", (socket) => {
 });
 
 // ── Express Middleware ─────────────────────────────────────────────────────────
+app.set("trust proxy", 1);
 app.use(helmet());
 app.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:3000",
